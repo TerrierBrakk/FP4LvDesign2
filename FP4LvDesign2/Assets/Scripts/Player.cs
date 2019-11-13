53a5dbbe64654c3920457c4c1718f97261afa3d1
Felipe <felipeortega94@gmail.com>
2019-11-13 08:39:51 -0600

    unity

---
+++ FP4LvDesign2/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController myController;
+    public float speed = 12.0f;
+
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public bool isGrounded;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+      
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement ()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f; 
+        }
+
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = transform.right * xMov + transform.forward * zMov;
+
+        myController.Move(movement * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        myController.Move(velocity * Time.deltaTime);
+        
+    }
+
+}
---
+++ FP4LvDesign2/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float mouseSensibility = 90.0f;
+    public Transform playerBody;
+    float xRotation = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseView();
+    }
+
+    void MouseView()
+    {
+        float mouseXAxis = Input.GetAxis("Mouse X") * mouseSensibility * Time.deltaTime;
+        float mouseYAxis = Input.GetAxis("Mouse Y") * mouseSensibility * Time.deltaTime;
+
+        xRotation -= mouseYAxis;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseXAxis);
+    }
+}
